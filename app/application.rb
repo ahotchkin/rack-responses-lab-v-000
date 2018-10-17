9940414c024eb393a1055b6a6e8a301f33f37549
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-10-17 23:37:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    if t.hour < 12
+      resp.write "Good Morning!"
+    if t.hour >= 12
+      resp.write "Good Afternoon!"
+    end
+    
     resp.finish
 end
